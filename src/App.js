28a817a81ef50fe71475ca8f6d767b836c805e08
Gregory Millare <gmillare4@gmail.com>
2021-02-16 17:11:42 -0600

    can make api requests for stock data

--- src/App.js
+++ src/App.js
@@ -2,7 +2,19 @@ import logo from "./logo.svg";
 import "./App.css";
 import { useEffect } from "react";
 
+const apiUrl = `https://api.polygon.io/v2/aggs/ticker/GME/range/1/minute/2020-10-14/2020-10-14?unadjusted=true&sort=asc&limit=50000&apiKey=yJzK3sWL5v0tUxElpFaT4O0ed8ZJzN58`;
+async function getStocks() {
+  const response = await fetch(apiUrl);
+  return response.json();
+}
+
 function App() {
+  useEffect(() => {
+    getStocks().then((data) => {
+      console.log(data);
+    });
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
