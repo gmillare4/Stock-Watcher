e2c2c0b0bf1983b286337e5101a07de6dff0fb6b
Gregory Millare <gmillare4@gmail.com>
2021-02-23 11:36:18 -0600

    added working express route

--- src/components/CandleView.js
+++ src/components/CandleView.js
@@ -3,7 +3,7 @@ import { React, useEffect, useState } from "react";
 export const CandleView = (props) => {
   const [ticker, setTicker] = useState("Ticker Symbol");
 
-  const apiUrl = `https://api.polygon.io/v2/aggs/ticker/${props.ticker}/range/1/minute/2020-10-14/2020-10-14?unadjusted=true&sort=asc&limit=50000&apiKey=yJzK3sWL5v0tUxElpFaT4O0ed8ZJzN58`;
+  const apiUrl = `https://api.polygon.io/v2/aggs/ticker/${props.ticker}/range/1/minute/2021-02-12/2021-02-12?unadjusted=true&sort=asc&limit=50000&apiKey=yJzK3sWL5v0tUxElpFaT4O0ed8ZJzN58`;
   async function getStocks() {
     const response = await fetch(apiUrl);
     return response.json();
@@ -15,8 +15,6 @@ export const CandleView = (props) => {
       setTicker(data.ticker);
     });
   }, []);
-
-  console.log("candleview");
 
   return (
     <div>
