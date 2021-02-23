4bb014a424940511c1e42b7b481cd7864b4650f6
Gregory Millare <gmillare4@gmail.com>
2021-02-23 16:52:51 -0600

    can dynamically request candles of the current day

--- src/components/CandleView.js
+++ src/components/CandleView.js
@@ -3,11 +3,14 @@ import { React, useEffect, useState } from "react";
 export const CandleView = (props) => {
   const [ticker, setTicker] = useState("Ticker Symbol");
 
-  const start = 1614069000 / 1000;
+  const marketOpen = new Date();
+  marketOpen.setHours(8);
+  marketOpen.setMinutes(30);
+  marketOpen.setSeconds(0);
+  marketOpen.setMilliseconds(0);
+  const start = Math.trunc(marketOpen / 1000);
 
   let end = Math.trunc(new Date() / 1000);
-
-  console.log("end", end);
 
   const apiUrl = `https://api.polygon.io/v2/aggs/ticker/${props.ticker}/range/1/minute/2021-02-12/2021-02-12?unadjusted=true&sort=asc&limit=50000&apiKey=yJzK3sWL5v0tUxElpFaT4O0ed8ZJzN58`;
   const finUrl = `https://finnhub.io/api/v1/stock/candle?symbol=${props.ticker}&resolution=1&from=${start}&to=${end}&token=c0m5rvn48v6rkav1k8u0`;
