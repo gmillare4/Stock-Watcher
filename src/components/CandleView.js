94bac3e592ae4a92cc36b4b7567611f63f4b0f04
Gregory Millare <gmillare4@gmail.com>
2021-02-23 12:46:19 -0600

    can get realtime trades from Finnhub via websockets

--- src/components/CandleView.js
+++ src/components/CandleView.js
@@ -9,6 +9,25 @@ export const CandleView = (props) => {
     return response.json();
   }
 
+  const socket = new WebSocket(
+    "wss://ws.finnhub.io?token=c0m5rvn48v6rkav1k8u0"
+  );
+
+  // Connection opened -> Subscribe
+  socket.addEventListener("open", function (event) {
+    socket.send(JSON.stringify({ type: "subscribe", symbol: "GME" }));
+  });
+
+  // Listen for messages
+  socket.addEventListener("message", function (event) {
+    console.log("Message from server ", event.data);
+  });
+
+  // Unsubscribe
+  const unsubscribe = function (symbol) {
+    socket.send(JSON.stringify({ type: "unsubscribe", symbol: symbol }));
+  };
+
   useEffect(() => {
     getStocks().then((data) => {
       console.log(data);
